d593b507e8d897323ad723d4e379f37c97fde589
Alexander Annenkov <55015984+axel-hk@users.noreply.github.com>
2021-11-09 14:26:58 +0300

    TransferOptimisticLog edit

--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferOptimisticLock.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferOptimisticLock.kt
@@ -17,26 +17,52 @@ class TransferOptimisticLock {
             val autoCommit = conn.autoCommit
             try {
                 conn.autoCommit = false
-                val prepareStatement1 = conn.prepareStatement("select amount from account1 where id = ${accountId1} for update")
-                prepareStatement1.use { statement ->
-                   val result =  statement.executeUpdate()
-                    if(result<amount) throw MyCustomException("На счету не хватает средств")
+
+
+                var version1 = 0
+                var version2 = 0
+                var currentAmount = 0L
+
+                val preparedStatement1 = conn.prepareStatement("select * from bank where id in (?, ?)")
+                preparedStatement1.use { statement ->
+                    statement.setLong(1, accountId1)
+                    statement.setLong(2, accountId2)
+                    statement.executeQuery().use { resultSet ->
+                        resultSet.next()
+                        currentAmount = resultSet.getLong("amount")
+                        version1 = resultSet.getInt("version")
+                        resultSet.next()
+                        version2 = resultSet.getInt("version")
+                    }
                 }
-                val prepareStatement2 = conn.prepareStatement("select amount from account1 where id = ${accountId2} for update")
-                prepareStatement2.use { statement ->
-                    statement.executeUpdate()
+
+                if (currentAmount - amount < 0) throw MyCustomException("Текущая сумма меньше суммы списания")
+
+                val preparedStatement2 =
+                    conn.prepareStatement("update bank set amount = amount - ?, version = version + 1 where id = ? and version = ?")
+                preparedStatement2.use { statement ->
+                    statement.setLong(1, amount)
+                    statement.setLong(2, accountId1)
+                    statement.setInt(3, version1)
+                    val updateRows = statement.executeUpdate()
+                    if (updateRows == 0) throw SQLException()
                 }
-                val prepareStatement3 = conn.prepareStatement("update account1 set amount = amount - $amount where id = ${accountId1}")
-                prepareStatement3.use { statement ->
-                    statement.executeQuery()
-                }
-                val prepareStatement4 = conn.prepareStatement("update account1 set amount = amount + $amount where id = ${accountId2}")
-                prepareStatement4.use { statement ->
-                    statement.executeQuery()
+                val preparedStatement3 =
+                    conn.prepareStatement("update bank set amount = amount + ?, version = version + 1 where id = ? and version = ?")
+                preparedStatement3.use { statement ->
+                    statement.setLong(1, amount)
+                    statement.setLong(2, accountId2)
+                    statement.setInt(3, version2)
+                    val updateRows = statement.executeUpdate()
+                    if (updateRows == 0) throw SQLException()
                 }
                 conn.commit()
-            } catch (exception: SQLException) {
+
+            } catch (exception: MyCustomException) {
                 println(exception.message)
+                conn.rollback()
+            } catch (exception: SQLException) {
+                println(exception.message )
                 conn.rollback()
             } finally {
                 conn.autoCommit = autoCommit
